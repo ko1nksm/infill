823bf5df21ba923484fc6d5e889f014f2f0e2a67
Koichi Nakashima <koichi@nksm.name>
2016-08-05 15:13:30 +0900

    Add gcloud

---
+++ gcloud/.zshrc.d/gcloud.sh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+source ~/opt/google-cloud-sdk/path.zsh.inc
+
+# The next line enables shell command completion for gcloud.
+source ~/opt/google-cloud-sdk/completion.zsh.inc
