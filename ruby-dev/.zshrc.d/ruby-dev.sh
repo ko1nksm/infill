926ac45bc44d0979ff659cbf815d3d8b24369609
Koichi Nakashima <koichi@nksm.name>
2017-05-11 10:49:29 +0900

    ruby-dev: remove RUBYOPT

--- ruby-dev/.zshrc.d/ruby-dev.sh
+++ ruby-dev/.zshrc.d/ruby-dev.sh
@@ -1,6 +1,6 @@
-RUBYOPT="-rpp"
-if gem list -i pry > /dev/null; then
-  RUBYOPT="$RUBYOPT -rpry"
-fi
-
-export RUBYOPT
+# RUBYOPT="-rpp"
+# if gem list -i pry > /dev/null; then
+#   RUBYOPT="$RUBYOPT -rpry"
+# fi
+#
+# export RUBYOPT
